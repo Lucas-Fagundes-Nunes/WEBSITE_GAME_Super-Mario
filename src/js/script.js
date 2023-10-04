40d9b1fa37a34406c1bb7e0de837ab9a569ad5b0
LucasNunes <lucasfnunes6@gmail.com>
2023-10-04 15:55:28 -0300

    Adicionado pontuação

--- src/js/script.js
+++ src/js/script.js
@@ -1,31 +1,48 @@
-const mario = document.querySelector('.mario');
-const pipe = document.querySelector('.pipe');
+let states = {
+    results: {
+        score: 0,
+    },
+    view: {
+        pipe: document.querySelector('.pipe'),
+        mario: document.querySelector('.mario'),
+        points: document.querySelector('.points-results'),
+        score: document.getElementById('score'),
+        
+    }
+}
 
 const jump = () => {
-    mario.classList.add('jump');
+    states.view.mario.classList.add('jump');
 
     setTimeout(() => {
-        mario.classList.remove('jump')
+        states.view.mario.classList.remove('jump');
     }, 500)
+    
 }
 
 const loop = setInterval(() => {
 
-    const pipePosition = pipe.offsetLeft;
-    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+    const pipePosition = states.view.pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(states.view.mario).bottom.replace('px', '');
     
     if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 95) {
 
-        pipe.style.animation = 'none';
-        pipe.style.left = `${pipePosition}px`;
+        states.view.pipe.style.animation = 'none';
+        states.view.pipe.style.left = `${pipePosition}px`;
 
-        mario.style.animation = 'none';
-        mario.style.bottom = `${marioPosition}px`;
+        states.view.mario.style.animation = 'none';
+        states.view.mario.style.bottom = `${marioPosition}px`;
         
-        mario.src = './src/images/game-over.png';
-        mario.style.width = '75px';
-        mario.style.marginLeft = '50px'
+        states.view.mario.src = './src/images/game-over.png';
+        states.view.mario.style.width = '75px';
+        states.view.mario.style.marginLeft = '50px';
+
+        states.view.points.classList.add('points');
+    } else {
+        states.results.score++; 
     }
+
+    states.view.score.innerText = states.results.score;
 
 }, 10);
 
